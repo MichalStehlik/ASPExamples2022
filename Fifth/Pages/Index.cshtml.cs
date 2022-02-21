cd6cc0cf76f25bc06f1344b80b8f633ba2a1527a
Michal Stehlík <michal.stehlik@pslib.cz>
2022-02-21 09:24:15 +0100

    forms 1

---
+++ Fifth/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fifth.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty] // anotace - annotation, dekorace - decoration, atribut
+        [Required(ErrorMessage = "Email musí být vyplněn, jinak ti vybouchne hlava")]
+        [MinLength(7, ErrorMessage = "Email musí mít aspoň 7 znaků")]
+        public string Email { get; set; } = "x@x.x";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Email = "b.b@b.b";
+        }
+
+        public ActionResult OnPost(/*string email*/)
+        {
+            // Email = Request.Form["email"];
+            // Email = email;
+            // return RedirectToPage("./Privacy");
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
